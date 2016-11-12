4a9bf5304824ec1a608f080ad3cb8c10fc30a83e
Sandor Gabor Kocso <kocso.sandor.gabor@gmail.com>
2016-11-12 10:37:48 +0100

    adding Neural Nets

---
+++ src/main/java/kits/ml/neuralnet/Neuron.java
@@ -0,0 +1,28 @@
+package kits.ml.neuralnet;
+
+import java.util.stream.IntStream;
+
+import kits.ml.core.Input;
+import kits.ml.core.math.MLMath;
+
+public class Neuron {
+
+	private final double[] weights;
+	
+	private final int inputDimension;
+	
+	public Neuron(int inputDimension) {
+		this.inputDimension = inputDimension;
+		weights = new double[inputDimension + 1];
+	}
+	
+	public Neuron(double ... weights) {
+		this.weights = weights;
+		inputDimension = weights.length - 1;
+	}
+	
+	public double calculateOutput(Input input) {
+		return MLMath.sigmoid(weights[0] + IntStream.range(0, inputDimension).mapToDouble(i -> weights[i+1] * input.values[i]).sum());
+	}
+	
+}
